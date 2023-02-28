6dbe270a3d19586197701758982b6d959bee20ba
araf821 <araf.ahmed200@gmail.com>
2023-02-28 00:10:57 -0500

    set up the initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Products from "./pages/ProductsPage";
+import SingleProduct from "./pages/SingleProductPage";
+import PrivateRoute from "./pages/PrivateRoute";
+import Checkout from "./pages/CheckoutPage";
+import Error from "./pages/Error";
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
+        <Route
+          path="checkout"
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        />
+        <Route path="error" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
