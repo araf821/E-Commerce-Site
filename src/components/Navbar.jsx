42fc925a6e369e451abbb862f4fe34f780fec657
araf821 <araf.ahmed200@gmail.com>
2023-02-28 23:13:39 -0500

    added navbar content and most of its styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,38 @@
+import { Link } from "react-router-dom";
+import { FaBars, FaShoppingCart, FaUserAlt } from "react-icons/fa";
+import { links } from "../data";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar-component">
+      <div className="nav-content">
+        <div className="nav-header">
+          <Link to="/">
+            <h1>BrandName</h1>
+          </Link>
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {links.map((link) => {
+            return (
+              <li key={link.id}>
+                <Link to={link.url}>{link.text}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="nav-buttons">
+          <button>
+            <FaShoppingCart />
+          </button>
+          <button>
+            <FaUserAlt />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
