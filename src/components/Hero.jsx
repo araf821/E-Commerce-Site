4312a63138a4e7c4e8859170a5396e386a9e7711
araf821 <araf.ahmed200@gmail.com>
2023-03-01 19:07:12 -0500

    implemented the hero section for the home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,25 @@
+import { Link } from "react-router-dom";
+import herobcg from "../assets/hero-bcg.jpeg";
+import herobcg2 from "../assets/hero-bcg-2.jpeg";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero section-center">
+      <article>
+        <h1>Design Your Comfort Zone</h1>
+        <p>
+          Airedale hard cheese mozzarella. Pecorino melted cheese port-salut
+          emmental babybel cheese and wine melted cheese manchego. Everyone
+          loves blue castello everyone loves fromage cheese slices airedale
+          cheddar cream cheese.
+        </p>
+        <Link to="/products">Shop Now</Link>
+      </article>
+      <div className="img-container">
+        <img src={herobcg} alt="background" className="img1" />
+        <img src={herobcg2} alt="background" className="img2" />
+      </div>
+    </section>
+  );
 };
 export default Hero;
