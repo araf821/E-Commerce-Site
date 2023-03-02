8ed07504078110cb2f9146ef4771afc01540155f
araf821 <araf.ahmed200@gmail.com>
2023-03-01 19:38:55 -0500

    set up the services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,33 @@
+import { services } from "../data";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="services section">
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            custom furniture <br /> built only for you
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum debitis consectetur reprehenderit non aliquam voluptates
+            dolore aut vero consequuntur.
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const { id, icon, title, text } = service;
+            return (
+              <article className="service" key={id}>
+                <span className="icon">{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Services;
