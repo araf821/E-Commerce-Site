86f7540ff7453e8a36d1b052595c8d7023f4bbf7
araf821 <araf.ahmed200@gmail.com>
2023-03-01 19:18:36 -0500

    placeholder feature section implemented

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,4 +1,17 @@
+import { Link } from "react-router-dom";
+
 const FeaturedProducts = () => {
-  return <div>FeaturedProducts</div>;
+  return <section className="section featured-section">
+     <div className='title'>
+        <h2>Featured Products</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center products'>
+        Featured Products
+      </div>
+      <Link to='/products' className='btn'>
+        All Products
+      </Link>
+  </section>;
 };
 export default FeaturedProducts;
