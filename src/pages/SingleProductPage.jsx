809c540729666d8257cd2d9c34d504c92ac88a9c
araf821 <araf.ahmed200@gmail.com>
2023-03-03 15:07:56 -0500

    fetched single product data to be displayed

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PageHero from "../components/PageHero";
+import { single_product_url } from "../data";
+
 const SingleProductPage = () => {
-  return <div>SingleProductPage</div>;
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+
+  const fetchProduct = async () => {
+    const response = await axios.get(`${single_product_url}${id}`);
+    setProduct(response.data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  return (
+    <main className="single-product-page">
+      <PageHero pageName={product.name} />
+    </main>
+  );
 };
 export default SingleProductPage;
