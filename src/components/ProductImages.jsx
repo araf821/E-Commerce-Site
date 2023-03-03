18ac121e651073419963280a899146bb4bf08718
araf821 <araf.ahmed200@gmail.com>
2023-03-03 17:35:46 -0500

    implemented single product images section

--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,4 +1,25 @@
-const ProductImages = () => {
-  return <div>ProductImages</div>;
+import { useState } from "react";
+
+const ProductImages = ({ images = [{ url: "" }] }) => {
+  const [mainImg, setMainImg] = useState(images[0]);
+
+  return (
+    <main className="product-images" >
+      <img src={mainImg.url} alt="main" className="main-img" />
+      <div className="bottom-images">
+        {images.map((image, index) => {
+          return (
+            <img
+              key={index}
+              src={image.url}
+              alt={image.filename}
+              onClick={() => setMainImg(images[index])}
+              className={`${image.url === mainImg.url ? "active" : null}`}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
 };
 export default ProductImages;
