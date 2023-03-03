f06d1de9a6c3dd1f70240dad731a2b1adf6f5741
araf821 <araf.ahmed200@gmail.com>
2023-03-02 21:32:25 -0500

    implemented use of formspree to sign up users for subscription

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,17 @@
+import { useForm, ValidationError } from "@formspree/react";
+
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xleknwpy");
+
+  if (state.succeeded) {
+    return <div className="contact-section">
+    <section className="submitted-form section section-center">
+      <h4>Form Received</h4>
+      <h3>Thanks for subscribing to our newsletter!</h3>
+    </section>
+  </div>;
+  }
+
   return (
     <div className="contact-section">
       <section className="section section-center">
@@ -7,9 +20,19 @@ const Contact = () => {
           Next level tbh everyday carry, blog copper mug forage kitsch roof
           party pickled hammock kale chips tofu.
         </p>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="email" name="email" placeholder="bebicat@gamil.com" />
-          <button>Subscribe</button>
+        <form
+        action="https://formspree.io/f/xleknwpy"
+        onSubmit={handleSubmit}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="bebicat@gmail.com"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <button type="submit" disabled={state.submitting}>
+            Subscribe
+          </button>
         </form>
       </section>
     </div>
