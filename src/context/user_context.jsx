4166ababb3188c8811411d71b66ebabfcae1e4dc
araf821 <araf.ahmed200@gmail.com>
2023-03-08 02:05:28 -0500

    set up auth0

--- src/context/user_context.jsx
+++ src/context/user_context.jsx
@@ -1,9 +1,25 @@
-import { createContext, useContext } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
+    useAuth0();
+
+  const [myUser, setMyUser] = useState(null);
+
+  useEffect(() => {
+    console.log("user", user);
+    console.log("isAuthenticated", isAuthenticated);
+    console.log("isLoading", isLoading);
+  }, [isAuthenticated]);
+
+  return (
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 // Custom hook
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import App from "./App";
 import { CartProvider } from "./context/cart_context";
 import { FilterProvider } from "./context/filter_context";
 import { ProductsProvider } from "./context/products_context";
 import "./index.scss";
 
+const auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const auth0ClientId = import.meta.env.VITE_AUTH0_CLIENTID;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain={auth0Domain}
+    clientId={auth0ClientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 );
