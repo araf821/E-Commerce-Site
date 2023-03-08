10b772442630473af552ca0161d89267a6133862
araf821 <araf.ahmed200@gmail.com>
2023-03-07 23:28:38 -0500

    implemented the overall cart structure setup

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+import { useCartContext } from "../context/cart_context";
+import CartSummary from "./CartSummary";
+
 const CartContent = () => {
-  return <div>asdfadf</div>;
+  const { cart, clearCart } = useCartContext();
+
+  return (
+    <main className="section-center">
+      {cart.map((item) => {
+        return <div>{item.name}</div>;
+      })}
+
+      <hr />
+      <div className="cart-buttons">
+        <Link to="/products">Continue Shopping</Link>
+        <button onClick={clearCart}>Clear Cart</button>
+      </div>
+      <CartSummary />
+    </main>
+  );
 };
 
 export default CartContent;
---
+++ src/components/CartSummary.jsx
@@ -0,0 +1,4 @@
+const CartSummary = () => {
+  return <div>CartSummary</div>;
+};
+export default CartSummary;
