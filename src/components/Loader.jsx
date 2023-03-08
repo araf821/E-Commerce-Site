820f5b8d752a659bb6fb8866e8ada9269c1bc9a2
araf821 <araf.ahmed200@gmail.com>
2023-03-08 18:48:08 -0500

    set up netlify cli and first serverless function

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,9 @@
 import loader from "../assets/loading.svg";
-import PageHero from "./PageHero";
 
 const Loader = () => {
   return (
     <main>
-      <PageHero pageName="Loading" />
-      <div className="page-100 loader section section-center">
+      <div className="loader section section-center">
         <img src={loader} alt="loader component" />
         <h4>Loading...</h4>
       </div>
